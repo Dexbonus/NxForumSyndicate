76d3aff19df08e36777e54b5b61ad3bfdee6b64f
Joseph-plex <joseph@plexxis.com>
2014-04-25 15:52:59 -0400

    Change Log
    - Changed Save result. Can now specific TextWriter or Stream to write too
    - Added a Name property to Tasks so it can be serialized easier.
    - Fixed a Typo where remove task was called Remove Tasks.

--- Plex.Diagnostics/Task.cs
+++ Plex.Diagnostics/Task.cs
@@ -1,15 +1,13 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Diagnostics;
+﻿using System.Diagnostics;
 
 namespace Plex.Diagnostics
 {
     public class Task
     {
         private Stopwatch timer;
-        public long duration;
+
+        public string Name { get; set; }
+        public long Duration { get; private set; }
 
         public Task()
         {
@@ -24,7 +22,7 @@ namespace Plex.Diagnostics
         public void Stop()
         {
             timer.Stop();
-            duration = timer.ElapsedTicks;
+            Duration = timer.ElapsedTicks;
         }
 
         public long GetMilliseconds()
