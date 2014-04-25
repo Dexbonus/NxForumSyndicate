86159a88a2fc34e3c333af64b2926797874c670f
Joseph-plex <joseph@plexxis.com>
2014-04-25 15:29:42 -0400

    Initially Commit

---
+++ Plex.Diagnostics/Task.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Diagnostics;
+
+namespace Plex.Diagnostics
+{
+    public class Task
+    {
+        private Stopwatch timer;
+        public long duration;
+
+        public Task()
+        {
+            timer = new Stopwatch();
+        }
+
+        public void Start()
+        {
+            timer.Start();
+        }
+
+        public void Stop()
+        {
+            timer.Stop();
+            duration = timer.ElapsedTicks;
+        }
+
+        public long GetMilliseconds()
+        {
+            return timer.ElapsedMilliseconds;
+        }
+
+        public override string ToString()
+        {
+            return timer.ElapsedTicks.ToString();
+        }
+    }
+
+}
---
+++ Plex.Diagnostics/TimeAnalyst.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+
+namespace Plex.Diagnostics
+{
+    public class TimeAnalyst
+    {
+        static Dictionary<String, Task> tasks = new Dictionary<String, Task>();
+
+        //Create
+        static public void CreateTask(String n)
+        {
+            tasks.Add(n,new Task());
+        }
+
+        //Read
+        static public Task GetTask(String n)
+        {
+            return tasks[n];
+        }
+
+        static public void SaveResults()
+        {
+            FileStream output = new FileStream("TimeAnalystResults.txt", FileMode.Create);
+            foreach (KeyValuePair<String, Task> kvp in tasks)
+            {
+                Byte[] b = Encoding.ASCII.GetBytes(kvp.Key + " : " + kvp.Value.ToString() + " Ticks\n");
+                output.Write(b, 0, b.Length);
+            }
+            output.Close();
+        }
+
+        //Update
+        static public void StartTask(String n)
+        {
+            if (!tasks.ContainsKey(n))
+                CreateTask(n);
+            tasks[n].Start();
+        }
+
+        static public void StopTask(String n)
+        {
+            tasks[n].Stop();
+        }
+
+        //delete
+        static public void RemoteTask(String n)
+        {
+            tasks.Remove(n);
+        }
+
+        
+    }
+}
